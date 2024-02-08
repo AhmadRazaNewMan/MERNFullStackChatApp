8d66a9b9bb2fd6ad894e9c8cd036fdc00c0cbd8d
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-02-08 09:49:21 +0500

    adding messages section now

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import SearchInput from './sidebar/SearchInput'
 import Conversations from './sidebar/Conversations'
+import LogoutButton from './sidebar/LogoutButton'
 
 
 const Sidebar = () => {
   return (
-    <div className='border-r border-slate-500 p-4'>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
     <SearchInput/>
     <div className='divider px-3'></div>
      <Conversations/>
-    {/* <Logout/> */} 
+    <LogoutButton/>
     </div>
   )
 }
---
+++ Frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BiLogOut } from 'react-icons/bi';
+
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto'>
+        <BiLogOut className=" text-white w-6 h-6 cursor-pointer"/>
+    </div>
+  );
+}
+
+export default LogoutButton;

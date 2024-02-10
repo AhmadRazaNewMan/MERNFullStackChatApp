7849bc1776c56a4313f6a06f315fe3ac736dabd9
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-02-10 11:53:14 +0500

    adding search functionality in sidebar

--- Frontend/src/components/sidebar/SearchInput.jsx
+++ Frontend/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import {React,useState} from 'react'
 import {IoSearchSharp} from 'react-icons/io5'
+import useConversation from  "../../zustand/useConversation"
+import useGetConversation from '../../hooks/useGetConversation'
+import toast from 'react-hot-toast'
 const SearchInput = () => {
+  const  [search, setSearch] = useState('')
+  const {setSelectedConversation} = useConversation()
+  const  {conversation} = useGetConversation(10)
+  const handleSubmit =(e)=>{
+   
+    e.preventDefault()
+    if(!search) return;
+    if(search.length < 3){
+      return toast.error ('Search term must be at least 3 characters long')
+
+    }
+    const conversations = conversation.find((c)=>c.fullName.toLowerCase().includes(search.toLowerCase()))
+    if(conversations){
+      setSelectedConversation(conversations);
+      setSearch("")
+
+    }
+    else{
+      toast.error("No such User Found!")
+    }
+
+  }
   return (
-    <form className='flex gap-2 items-center'>
-        <input type='text' placeholder='Search....' className='input input-borderd rounded-full' >
+    <form onSubmit={handleSubmit} className='flex gap-2 items-center'>
+        <input type='text' placeholder='Search....'value={search} onChange={(e)=>setSearch(e.target.value)} className='input input-borderd rounded-full' >
         </input>
         <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
             <IoSearchSharp className="w-6 h-6 outline-none">
--- Frontend/src/hooks/useGetMessages.js
+++ Frontend/src/hooks/useGetMessages.js
@@ -21,7 +21,7 @@ const useGetMessages = () => {
 			}
 		};
 
-		if (selectedConversation?._id) getMessages();
+		if (selectedConversation?._id) getMessages();	
 	}, [selectedConversation?._id, setMessages]);
 
 	return { messages, loading };

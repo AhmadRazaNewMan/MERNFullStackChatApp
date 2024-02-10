cec49dd454c8bd1442ae19ed10a1d73ceb8d8004
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-02-10 14:40:13 +0500

    deployment setup on render

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser";
+import path from "path"
 
 import messageRoutes from "./routes/message.route.js";
 import authRoutes from './routes/auth.routes.js'
@@ -14,10 +15,18 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000 ;
 
+const __dirname =path.resolve()
+
 app.use(express.json())
 app.use('/api/auth',authRoutes) 
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/Frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "Frontend","dist","index.html"))
+})
 
 
 //    Server Listening
